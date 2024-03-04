abfaa8775864660cb4a36f33c97b426411968e10
CamillaLucero23 <camillalucero23@gmail.com>
2024-03-04 11:55:24 -0700

    Finished Edit

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
 
     # Render index.html
-    return render( request, 'portfolio_app/index.html')
+    student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
+    print("actice port. query set", student_active_portfolios)
+    return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
